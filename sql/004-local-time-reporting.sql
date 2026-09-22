e211862bfe4a5d5e42d9ae482562a623a08da5e6
chida <christian@damberg.org>
2026-09-22 14:00:06 +0200

    Refactor SQL script for local time reporting functions

--- sql/004-local-time-reporting.sql
+++ sql/004-local-time-reporting.sql
@@ -1,11 +1,10 @@
 SET NOCOUNT ON;
 SET XACT_ABORT ON;
 
--- Change this value and re-run the script to set another default reporting timezone.
 DECLARE @DefaultLocalTimeZone sysname = N'W. Europe Standard Time';
 DECLARE @EscapedDefaultLocalTimeZone nvarchar(256) = REPLACE(@DefaultLocalTimeZone, '''', '''''');
 
-DECLARE @CreateObjectsSql nvarchar(max) = N'
+DECLARE @Sql1 nvarchar(max) = N'
 CREATE OR ALTER FUNCTION dbo.ufn_ToastMessageLocal
 (
     @TimeZoneName sysname = N''' + @EscapedDefaultLocalTimeZone + N'''
@@ -24,8 +23,9 @@ RETURN
         m.CreatedUtc AT TIME ZONE ''UTC'' AT TIME ZONE @TimeZoneName AS CreatedLocalTime,
         m.ExpiresUtc AT TIME ZONE ''UTC'' AT TIME ZONE @TimeZoneName AS ExpiresLocalTime
     FROM dbo.ToastMessage m
-);
+);';
 
+DECLARE @Sql2 nvarchar(max) = N'
 CREATE OR ALTER FUNCTION dbo.ufn_ToastDeliveryLocal
 (
     @TimeZoneName sysname = N''' + @EscapedDefaultLocalTimeZone + N'''
@@ -47,8 +47,9 @@ RETURN
         c.LastSeenUtc AT TIME ZONE ''UTC'' AT TIME ZONE @TimeZoneName AS LastSeenLocalTime
     FROM dbo.ToastDelivery d
     INNER JOIN dbo.ToastClient c ON c.ClientId = d.ClientId
-);
+);';
 
+DECLARE @Sql3 nvarchar(max) = N'
 CREATE OR ALTER VIEW dbo.vw_ToastMessageLocal
 AS
 SELECT
@@ -60,8 +61,9 @@ SELECT
     m.ExpiresUtc,
     m.CreatedUtc AT TIME ZONE ''UTC'' AT TIME ZONE N''' + @EscapedDefaultLocalTimeZone + N''' AS CreatedLocalTime,
     m.ExpiresUtc AT TIME ZONE ''UTC'' AT TIME ZONE N''' + @EscapedDefaultLocalTimeZone + N''' AS ExpiresLocalTime
-FROM dbo.ToastMessage m;
+FROM dbo.ToastMessage m;';
 
+DECLARE @Sql4 nvarchar(max) = N'
 CREATE OR ALTER VIEW dbo.vw_ToastDeliveryLocal
 AS
 SELECT
@@ -78,4 +80,7 @@ SELECT
 FROM dbo.ToastDelivery d
 INNER JOIN dbo.ToastClient c ON c.ClientId = d.ClientId;';
 
-EXEC sp_executesql @CreateObjectsSql;
+EXEC sp_executesql @Sql1;
+EXEC sp_executesql @Sql2;
+EXEC sp_executesql @Sql3;
+EXEC sp_executesql @Sql4;
